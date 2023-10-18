04440d530860eca6139f5e76155323e4df38d7a6
WinterHana <102120762+WinterHana@users.noreply.github.com>
2023-10-18 21:59:17 +0900

    [level unrated] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int length = num_list.length;
+        for(int i = 0; i < length / 2; i++) {
+            int temp = num_list[i];
+            num_list[i] = num_list[length - 1 - i];
+            num_list[length - 1 - i] = temp;
+        }
+        
+        return num_list;
+    }
+}
