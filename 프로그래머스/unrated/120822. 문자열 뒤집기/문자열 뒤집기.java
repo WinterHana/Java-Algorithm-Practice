26503ab2b91b9541bcf528abe928f6edce348e0a
WinterHana <102120762+WinterHana@users.noreply.github.com>
2023-10-18 23:36:15 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder(my_string);
+        
+        sb = sb.reverse();
+        
+        return sb.toString();
+        
+    }
+}
