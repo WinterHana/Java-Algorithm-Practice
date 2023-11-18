9c17bca5edbf36ed6277f3b77ba6c3b8ae6a5926
WinterHana <102120762+WinterHana@users.noreply.github.com>
2023-11-18 17:03:51 +0900

    [Bronze II] Title: 수 정렬하기, Time: 196 ms, Memory: 15608 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,32 +1,27 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 public class Main {
-	public void solution() throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		
-		int n = Integer.parseInt(br.readLine());
-		int[] list = new int[n];
-		
-		for(int i = 0; i < n; i++) {
-			list[i] = Integer.parseInt(br.readLine());
-		}
-		
-		Arrays.sort(list);
-		
-		for(int val : list) {
-			bw.write(String.valueOf(val) + "\n");
-		}
-		bw.flush();
-		bw.close();
-	} 
-		
 	public static void main(String[] args) throws Exception {
-		new Main().solution();
- 	}
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = i + 1; j < N; j++) {
+				if(arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for(int i = 0; i < N; i++) {
+			System.out.println(arr[i]);
+		}
+	}
 }
