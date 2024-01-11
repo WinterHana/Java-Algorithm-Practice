78e207b98f6162931241cf726035602fe601dd22
WinterHana <102120762+WinterHana@users.noreply.github.com>
2024-01-11 20:57:27 +0900

    [Gold IV] Title: 좋다, Time: 192 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -4,36 +4,37 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		long[] arr = new long[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
-			arr[i] = Long.parseLong(st.nextToken());
-		}
-		
-		Arrays.sort(arr);
-		
-		int result = 0;
-		for(int k = 0; k < n; k++) {
-			 int i = 0;
-			 int j = n - 1;
-			 long point = arr[k];
-			while(i < j) {
-				 if(arr[i] + arr[j] > point) j--;
-				 else if(arr[i] + arr[j] < point) i++;
-				 else {
-					 if(i != k && j != k) {
-						 result++;
-						 break;
-					 }
-					 else if(k == i) i++;
-					 else if(k == j) j--;
-				 }
-			}
-		}
-		System.out.println(result);
-		br.close();
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] arr = new long[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int count = 0;
+        for (int k = 0; k < N; k++) {
+            int i = 0, j = N - 1;
+            while (i < j) {
+                if (arr[i] + arr[j] > arr[k]) {
+                    j--;
+                } else if (arr[i] + arr[j] < arr[k]) {
+                    i++;
+                } else {
+                    if (i != k && j != k) {
+                        count++;
+                        break;
+                    } else if (i == k) {
+                        i++;
+                    } else if (j == k) {
+                        j--;
+                    }
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
 }
