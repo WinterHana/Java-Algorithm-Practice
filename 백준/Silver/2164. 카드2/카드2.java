e5f57dd9ff8c9dec41164b2f6d6766750bc42ed4
WinterHana <102120762+WinterHana@users.noreply.github.com>
2024-01-14 15:42:50 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 53368 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.remove();
+            if (queue.size() <= 1) break;
+
+            int k = queue.remove();
+            queue.add(k);
+        }
+
+        System.out.println(queue.peek());
+    }
+}
