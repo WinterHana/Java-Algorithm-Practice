86c3a6c48ecfd6a8eb885e7d2c3631cca70a5988
WinterHana <102120762+WinterHana@users.noreply.github.com>
2024-08-22 15:19:47 +0900

    [Silver I] Title: 미로 탐색, Time: 148 ms, Memory: 16724 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,71 +1,97 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
+/**
+ * 괴물이 있는 부분 : 0
+ * 괴물이 없는 부분 : 1
+ * 시작칸과 마지막 칸은 항상 1
+ * 칸을 셀 때는 시작 칸과 마지막 칸을 모두 포함해서 계산한다.
+ */
 class Point {
     int x;
     int y;
 
-    public Point(int x, int y) {
+    Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
-class Main {
-    static int[][] miro;
 
-    static int bfs(int N, int M) {
-        LinkedList<Point> queue = new LinkedList<>();
-        queue.add(new Point(0, 0));
+public class Main {
+    public static int[][] graph;
+    public static LinkedList<Point> queue;
+
+    public static int bfs(int N, int M) {
+        queue = new LinkedList<>();
+        Point start = new Point(0, 0);
+        queue.add(start);
+        int count = 1;
 
         while(!queue.isEmpty()) {
             Point p = queue.poll();
 
-            if(p.x - 1 >= 0 && miro[p.x - 1][p.y] == 1) {
-                miro[p.x - 1][p.y] = miro[p.x][p.y] + 1;
+//            if(p.x - 1 >= 0 && graph[p.x - 1][p.y] == 1) {
+//                graph[p.x - 1][p.y] = ++count;
+//                queue.add(new Point(p.x - 1, p.y));
+//            } else if(p.x + 1 < N && graph[p.x + 1][p.y] == 1) {
+//                graph[p.x + 1][p.y] = ++count;
+//                queue.add(new Point(p.x + 1, p.y));
+//            } else if(p.y - 1 >= 0 && graph[p.x][p.y - 1] == 1) {
+//                graph[p.x][p.y - 1] = ++count;
+//                queue.add(new Point(p.x, p.y - 1));
+//            } else if(p.y + 1 < M && graph[p.x][p.y + 1] == 1) {
+//                graph[p.x][p.y + 1] = ++count;
+//                queue.add(new Point(p.x, p.y + 1));
+//            }
+
+//            if(p.x + 1 < N && graph[p.x + 1][p.y] == 1) {
+//                graph[p.x + 1][p.y] = ++count;
+//                queue.add(new Point(p.x + 1, p.y));
+//            }  else if(p.y + 1 < M && graph[p.x][p.y + 1] == 1) {
+//                graph[p.x][p.y + 1] = ++count;
+//                queue.add(new Point(p.x, p.y + 1));
+//            }
+
+            if(p.x - 1 >= 0 && graph[p.x - 1][p.y] == 1) {
+                graph[p.x - 1][p.y] = graph[p.x][p.y] + 1;
                 queue.add(new Point(p.x - 1, p.y));
             }
-            if(p.x + 1 < N && miro[p.x + 1][p.y] == 1) {
-                miro[p.x + 1][p.y] = miro[p.x][p.y] + 1;
+            if(p.x + 1 < N && graph[p.x + 1][p.y] == 1) {
+                graph[p.x + 1][p.y] = graph[p.x][p.y] + 1;
                 queue.add(new Point(p.x + 1, p.y));
             }
-            if(p.y - 1 >= 0 && miro[p.x][p.y - 1] == 1) {
-                miro[p.x][p.y - 1] = miro[p.x][p.y] + 1;
+            if(p.y - 1 >= 0 && graph[p.x][p.y - 1] == 1) {
+                graph[p.x][p.y - 1] = graph[p.x][p.y] + 1;
                 queue.add(new Point(p.x, p.y - 1));
             }
-            if(p.y + 1 < M && miro[p.x][p.y + 1] == 1) {
-                miro[p.x][p.y + 1] = miro[p.x][p.y] + 1;
+            if(p.y + 1 < M && graph[p.x][p.y + 1] == 1) {
+                graph[p.x][p.y + 1] = graph[p.x][p.y] + 1;
                 queue.add(new Point(p.x, p.y + 1));
             }
+
         }
 
-        return miro[N - 1][M - 1];
+        return graph[N - 1][M - 1];
     }
-
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        miro = new int[N][M];
+        graph = new int[N][M];
 
         for(int i = 0; i < N; i++) {
             String[] line = br.readLine().split("");
             for(int j = 0; j < M; j++) {
-                miro[i][j] = Integer.parseInt(line[j]);
+                graph[i][j] = Integer.parseInt(line[j]);
             }
         }
 
-        System.out.println(bfs(N, M));
-
-//        for(int i = 0; i < N; i++) {
-//            for(int j = 0; j < M; j++) {
-//                System.out.print(miro[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
+        int result = bfs(N, M);
+        System.out.println(result);
     }
 }
