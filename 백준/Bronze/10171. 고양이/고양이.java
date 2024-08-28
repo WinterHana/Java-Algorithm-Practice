571aff61ffd22ce3a43364ac8d2fe45fb1194810
WinterHana <102120762+WinterHana@users.noreply.github.com>
2024-08-28 23:35:57 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuffer sb = new StringBuffer();
+        sb.append("\\    /\\ \n");
+        sb.append(" )  ( ') \n");
+        sb.append("(  /  ) \n");
+        sb.append(" \\(__)| \n");
+
+        System.out.println(sb);
+    }
+}
