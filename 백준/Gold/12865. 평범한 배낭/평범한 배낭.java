0dead27098fe113e1659a6b60836ca031b5c1cbd
WinterHana <102120762+WinterHana@users.noreply.github.com>
2024-10-04 15:13:18 +0900

    [Gold V] Title: 평범한 배낭, Time: 172 ms, Memory: 53824 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[N + 1][K + 1];
+
+        int[] weights = new int[N + 1];
+        int[] values = new int[N + 1];
+
+        for(int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            weights[i] = Integer.parseInt(st.nextToken());
+            values[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= K; j++) {
+                if(weights[i] > j) dp[i][j] = dp[i - 1][j];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weights[i]] + values[i]);
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+}
