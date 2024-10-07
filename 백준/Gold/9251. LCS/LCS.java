28e38ad47c38894e44953855186b9c08c2cd278f
WinterHana <102120762+WinterHana@users.noreply.github.com>
2024-10-07 18:13:57 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 18928 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -10,15 +10,18 @@ public class Main {
         int s1Len = s1.length;
         int s2Len = s2.length;
 
+        // DP를 사용하기 위한 배열
         int[][] dp = new int[s1Len + 1][s2Len + 1];
 
+        // LCS 관련 점화식 설정
         for(int i = 1; i <= s1Len; i++) {
             for(int j = 1; j <= s2Len; j++) {
                 if(s1[i - 1].equals(s2[j - 1])) dp[i][j] = dp[i - 1][j - 1] + 1;
                 else dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
             }
         }
-
+        
+        // 디버깅
 //        for(int i = 0; i <= s1Len; i++) {
 //            for(int j = 0; j <= s2Len; j++) {
 //                System.out.print(dp[i][j] + " ");
@@ -26,6 +29,7 @@ public class Main {
 //            System.out.println();
 //        }
 
+        // 결과 출력
         System.out.println(dp[s1Len][s2Len]);
     }
 }
